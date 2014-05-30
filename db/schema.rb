106b793290c443e5ded4b5c98b2e0d2313e36f16
Mark McConachie <mark@mcconachie.me>
2014-05-31 00:53:04 +0200

    Generate repositories

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140530225119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "repositories", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "source"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
